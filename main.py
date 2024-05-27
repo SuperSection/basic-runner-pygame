8df7a30401817b34cc785d42b535f7064a936706
SuperSection <soumosarkar297@gmail.com>
2024-05-27 23:30:57 +0530

    Player character: keyboard input, jumping & gravity, creating floor (base condition)

--- main.py
+++ main.py
@@ -19,14 +19,22 @@ snail_rect = snail_surf.get_rect(midbottom = (600, 300))
 player_surf = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80, 300))
 
+player_gravity = 0
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        # if event.type == pygame.MOUSEMOTION:
-        #     if player_rect.collidepoint(event.pos): print("collision")
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                player_gravity = -20
+                    
+        if event.type == pygame.KEYDOWN and player_rect.bottom >= 300:
+            if event.key == pygame.K_SPACE:
+                player_gravity = -20
+        
             
     
     screen.blit(sky_surface, (0, 0))
@@ -40,7 +48,14 @@ while True:
     if snail_rect.right <= 0: snail_rect.left = 800
     screen.blit(snail_surf, snail_rect)
 
+    # Player
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 300: player_rect.bottom = 300
     screen.blit(player_surf, player_rect)
+    
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_SPACE]: print("space")
     
 
     pygame.display.update()
