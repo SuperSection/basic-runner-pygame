4c863b9604bbed3b0fffbdff71ba510a25769777
SuperSection <soumosarkar297@gmail.com>
2024-05-27 20:52:17 +0530

    implement custom color (rgb or hexadecimal)

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 sky_surface = pygame.image.load("graphics/Sky.png").convert()
 ground_surface = pygame.image.load("graphics/Ground.png").convert()
 
-score_surf = test_font.render("My game", False, "Black")
+score_surf = test_font.render("My game", False, (64,64,64))
 score_rect = score_surf.get_rect(center = (400, 50))
 
 snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
@@ -25,38 +25,23 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEBUTTONUP:
-            print(event.pos)
-        if event.type == pygame.MOUSEMOTION:
-            player_rect.collidepoint(event.pos)
-
-          
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos): print("collision")
+            
     
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     
-    pygame.draw.rect(screen, "Pink", score_rect)
-    pygame.draw.rect(screen, "Pink", score_rect, 10)
+    pygame.draw.rect(screen, "#c0e8ec", score_rect)
+    pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
     screen.blit(score_surf, score_rect)
-    
-    
-    pygame.draw.ellipse(screen, "Brown", pygame.Rect(50,200,100,100))
-    pygame.draw.circle(screen, "Blue", (400, 100), 30)
-    pygame.draw.line(screen, "Gold", (0, 0), pygame.mouse.get_pos(), 5)
-    
-    
+
     snail_rect.x -= 4
     if snail_rect.right <= 0: snail_rect.left = 800
     screen.blit(snail_surf, snail_rect)
 
     screen.blit(player_surf, player_rect)
     
-    # if player_rect.colliderect(snail_rect):
-    #     print("collision")
-    
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint(mouse_pos):
-    #     print(pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(60)
