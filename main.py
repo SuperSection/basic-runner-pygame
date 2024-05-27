602f288ca7fe64302422d793b8684f3a6ae8588f
SuperSection <soumosarkar297@gmail.com>
2024-05-27 20:43:55 +0530

    use of pygame.draw shapes

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
 sky_surface = pygame.image.load("graphics/Sky.png").convert()
 ground_surface = pygame.image.load("graphics/Ground.png").convert()
-text_surface = test_font.render("My game", False, "Black")
+
+score_surf = test_font.render("My game", False, "Black")
+score_rect = score_surf.get_rect(center = (400, 50))
 
 snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom = (600, 300))
@@ -27,12 +29,21 @@ while True:
             print(event.pos)
         if event.type == pygame.MOUSEMOTION:
             player_rect.collidepoint(event.pos)
-            
+
           
     
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (350, 50))
+    
+    pygame.draw.rect(screen, "Pink", score_rect)
+    pygame.draw.rect(screen, "Pink", score_rect, 10)
+    screen.blit(score_surf, score_rect)
+    
+    
+    pygame.draw.ellipse(screen, "Brown", pygame.Rect(50,200,100,100))
+    pygame.draw.circle(screen, "Blue", (400, 100), 30)
+    pygame.draw.line(screen, "Gold", (0, 0), pygame.mouse.get_pos(), 5)
+    
     
     snail_rect.x -= 4
     if snail_rect.right <= 0: snail_rect.left = 800
